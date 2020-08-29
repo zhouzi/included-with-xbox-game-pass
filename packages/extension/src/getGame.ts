d239d62588420a005038ff601571746296d51c0b
gabinaureche <hello@gabinaureche.com>
2020-08-29 16:36:51 +0200

    Improve matching

--- packages/extension/src/getGame.ts
+++ packages/extension/src/getGame.ts
@@ -4,14 +4,21 @@ import getGames from "./getGames";
 
 export default async function getGame(name: string): Promise<APIGame | null> {
   const games = await getGames();
-  const fuse = new Fuse(games, {
-    keys: ["name"],
-    includeScore: true,
-    shouldSort: true,
-  });
-  const matches = fuse
-    .search(name)
-    .filter((match) => match.item.availability.pc && match.score! < 0.4);
+  const fuse = new Fuse(
+    games.filter((game) => game.availability.pc),
+    {
+      keys: ["name"],
+      includeScore: true,
+      shouldSort: true,
+    }
+  );
+  const matches = fuse.search(name).filter(
+    (match) =>
+      match.score! <= 0.1 ||
+      // There's currently a bug with fuse.js for exact matches
+      // https://github.com/krisk/Fuse/issues/481
+      match.item.name.toLowerCase() === name.toLowerCase()
+  );
 
   return matches[0]?.item;
 }

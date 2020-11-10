bf8fc513620190c4025ed6d4abd434f197a4661c
gabinaureche <hello@gabinaureche.com>
2020-11-10 21:54:40 +0100

    Improve sync

--- packages/xgp.community/pages/api/v1/games/sync.ts
+++ packages/xgp.community/pages/api/v1/games/sync.ts
@@ -39,12 +39,24 @@ export default async (req: NextApiRequest, res: NextApiResponse<Game[]>) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const games: Record<string, Game> = {};
-  const finalGames: Game[] = [];
+  const expectations: { games: number | null; pages: number | null } = {
+    games: null,
+    pages: null,
+  };
 
   await page.goto(XBOX_GAME_PASS_URL);
 
   await (async function extractCurrentPage(): Promise<void> {
     await page.waitForSelector(SELECTORS.games);
+
+    if (expectations.games == null || expectations.pages == null) {
+      expectations.games = await page.$eval(SELECTORS.totalGames, (element) =>
+        Number(element.textContent!.match(/([0-9]+) result/)![1])
+      );
+      expectations.pages = await page.$$eval(SELECTORS.pages, (elements) =>
+        Number(elements[elements.length - 1].getAttribute("data-topage"))
+      );
+    }
 
     (
       await page.$$eval(
@@ -80,9 +92,32 @@ export default async (req: NextApiRequest, res: NextApiResponse<Game[]>) => {
     });
 
     try {
+      const min = 500;
+      const max = 2000;
+      const timeout = Math.round(Math.random() * (max - min) + min);
+      await page.waitForTimeout(timeout);
+
       await page.click(SELECTORS.next);
       return extractCurrentPage();
     } catch (err) {
+      const currentPage = await page.$eval(SELECTORS.currentPage, (element) =>
+        Number(element.getAttribute("data-topage")!)
+      );
+      if (
+        currentPage !== expectations.pages ||
+        Object.keys(games).length !== expectations.games
+      ) {
+        throw new Error(
+          `The script ended without meeting the expectations of ${
+            expectations.pages
+          } pages (${currentPage}) and ${expectations.games} games (${
+            Object.keys(games).length
+          }).`
+        );
+      }
+
+      console.log(`Scrapped ${Object.keys(games).length} games.`);
+
       const client = new faunadb.Client({
         secret: process.env.FAUNADB_SECRET,
       });
@@ -91,11 +126,22 @@ export default async (req: NextApiRequest, res: NextApiResponse<Game[]>) => {
         const game = games[id];
 
         try {
-          await client.query(getGameByID(game.id));
-
           const { id, addedAt, removedAt, ...properties } = game;
-          await client.query(updateGame(game.id, properties));
+          const { data: currentGame } = await client.query<{ data: Game }>(
+            getGameByID(game.id)
+          );
+
+          if (currentGame.removedAt) {
+            console.log(`${game.name} was added back.`);
+            await client.query(
+              updateGame(game.id, { ...properties, addedAt, removedAt: null })
+            );
+          } else {
+            console.log(`${game.name} was updated.`);
+            await client.query(updateGame(game.id, properties));
+          }
         } catch (err) {
+          console.log(`${game.name} was added.`);
           await client.query(createGame(game));
         }
       }
@@ -105,13 +151,12 @@ export default async (req: NextApiRequest, res: NextApiResponse<Game[]>) => {
       }>(getGames());
       for (const currentGame of currentGames) {
         if (games[currentGame.data.id] == null) {
+          console.log(`${currentGame.data.name} was removed.`);
           await client.query(
             updateGame(currentGame.data.id, {
               removedAt: addedAt,
             })
           );
-        } else {
-          finalGames.push(currentGame.data);
         }
       }
     }
@@ -119,5 +164,5 @@ export default async (req: NextApiRequest, res: NextApiResponse<Game[]>) => {
 
   browser.close();
 
-  res.status(200).json(finalGames);
+  res.status(200).json([]);
 };

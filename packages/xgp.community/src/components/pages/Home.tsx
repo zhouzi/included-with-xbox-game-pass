72ba006f4e501df91e3e7a162e0194a2dbecbf50
gabinaureche <hello@gabinaureche.com>
2020-11-14 15:54:38 +0100

    Add missing spaces

--- packages/xgp.community/src/components/pages/Home.tsx
+++ packages/xgp.community/src/components/pages/Home.tsx
@@ -97,18 +97,18 @@ export function Home() {
       </main>
       <footer className="Container Block Footer">
         <p className="Paragraph">
-          xgp.community is an
+          xgp.community is an{" "}
           <a
             href="https://github.com/zhouzi/included-with-xbox-game-pass"
             className="Link"
           >
             open source
-          </a>
-          side project by
+          </a>{" "}
+          side project by{" "}
           <a href="https://gabinaureche.com" className="Link">
             Gabin
           </a>
-          . Do not hesitate to get in touch by sending me at email at
+          . Do not hesitate to get in touch by sending me at email at{" "}
           <a href="mailto:gabin@xgp.community" className="Link">
             gabin@xgp.community
           </a>
@@ -116,15 +116,15 @@ export function Home() {
         </p>
         <p className="Paragraph">
           Also, the best way to be part of the community is by joining the Xbox
-          Game Pass subreddit:
+          Game Pass subreddit:{" "}
           <a href="https://reddit.com/r/xboxgamepass" className="Link">
             /r/xboxgamepass
           </a>
-          . Regarding the latest news, Microsoft usually posts them on
+          . Regarding the latest news, Microsoft usually posts them on{" "}
           <a href="https://twitter.com/xboxgamepass" className="Link">
             Twitter
           </a>
-          and/or on their
+          and/or on their{" "}
           <a
             href="https://news.xbox.com/en-us/xbox-game-pass/"
             className="Link"

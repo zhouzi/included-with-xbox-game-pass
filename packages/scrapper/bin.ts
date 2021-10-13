14704fcf72c5a36fa25f48afe184657208d6d508
gabinaureche <hello@gabinaureche.com>
2021-10-13 12:09:13 +0200

    Git ignore legacy list

--- packages/scrapper/bin.ts
+++ packages/scrapper/bin.ts
@@ -35,24 +35,6 @@ interface Game {
   );
 
   await fse.writeJSON(outputPath, games, { spaces: 2 });
-
-  await fse.writeJSON(
-    path.join(__dirname, "..", "website", "static", "games.json"),
-    games.map((game) => ({
-      slug: game.slug,
-      name: game.name,
-      availability: {
-        console: null,
-        pc: game.xboxUrl,
-        steam: game.steamId
-          ? `https://store.steampowered.com/app/${game.steamId}/`
-          : null,
-      },
-      steam: game.steamId,
-      updatedAt: "2021-01-01T00:00:00.000Z",
-    })),
-    { spaces: 2 }
-  );
 })();
 
 async function scrapGames() {
